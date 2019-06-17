4a68ba120a451caa72192cb6ef71f9597726f120
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-17 15:09:25 -0700

    use promisify to turn into fs functions into promises

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 'use strict';
 
-const fs = require('fs');
+const toRead = require('./src/readFile');
 const toUpper = require('./src/toUpper');
+const toWrite = require('./src/writeFile');
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
+  // fs.readFile( file, (err, data) => {
+  //   if(err) { throw err; }
+  //     let text = toUpper(data);
+  //   fs.writeFile( file, Buffer.from(text), (err, data) => {
+  //     if(err) { throw err; }
+  //     console.log(`${file} saved`);
+  //   });
 
-    let text = toUpper(data);
+  // });
 
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
+
+  toRead(file)
+    .then(data => {
+      toWrite(file, Buffer.from(toUpper(data)))
+    })
+    .then(console.log(`${file} saved`))
 };
 
 let file = process.argv.slice(2).shift();
