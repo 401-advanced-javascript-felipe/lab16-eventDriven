fce393e2513402176b9fd18d87a39d96831227a6
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-17 14:28:42 -0700

    modulirize toUpper

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const fs = require('fs');
+const toUpper = require('./src/toUpper');
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
     if(err) { throw err; }
-    let text = data.toString().toUpperCase();
+
+    let text = toUpper(data);
+
     fs.writeFile( file, Buffer.from(text), (err, data) => {
       if(err) { throw err; }
       console.log(`${file} saved`);
