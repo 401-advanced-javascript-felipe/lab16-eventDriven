1ac4d63c084c55783ab1a08c8cfb80d75ad4e5bd
felipedelatorre <felipede_latorre@hotmail.com>
2019-06-17 18:56:38 -0700

    fix indents

--- app.js
+++ app.js
@@ -3,20 +3,19 @@
 const toRead = require('./src/readFile');
 const toUpper = require('./src/toUpper');
 const toWrite = require('./src/writeFile');
-const eventLog = require('./src/events/eventLog');
-const eventError = require('./src/events/eventError');
+const eventLog = require('./src/events/event');
 require('./src/events/logger');
-require('./src/events/errorHandler');
+
+
 
 const alterFile = (file) => {
-
   toRead(file)
     .then(data => {
       toWrite(file, Buffer.from(toUpper(data)));
     })
     .then(eventLog.emit('log', file))
     .catch(error =>{
-      eventError.emit('error', error);
+      eventLog.emit('error', error);
     });
 };
 
--- src/events/logger.js
+++ src/events/logger.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const events = require('./eventLog');
+const events = require('./event');
 
 const logger = module.exports = exports = {};
+
 
 logger.log = (file) => {
   console.log(`${file} saved`);
 };
 
+logger.error = (file) => {
+  console.log(`${file} not saved`);
+};
+
 events.on('log', logger.log);
+events.on('error', logger.error);
